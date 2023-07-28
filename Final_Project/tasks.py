c3fa35a0ce2ad4645155213d9828ea8c2ebbb78e
Cosmin445 <cosminaursulesei13@gmail.com>
2023-07-28 16:47:59 +0300

    commit 15

--- Final_Project/tasks.py
+++ Final_Project/tasks.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, app, request, render_template
 from model import Task
 from peewee import JOIN
 
@@ -9,3 +9,23 @@ def get_users():
     tasks = list(Task.select())
     return render_template('tasks.html', tasks=tasks)
 
+@task_bp.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task_by_id(task_id):
+    task = Task.get_or_none(Task.task_id == task_id)
+    if task:
+        return render_template('task.html', task=task)
+    else:
+        return jsonify({"error": "Task not found."}), 404
+
+@task_bp.route('/submit_task', methods=['POST'])
+def submit_task():
+    task_title = request.form.get('task_title')
+    task_description = request.form.get('task_description')  
+    task = Task(task_title=task_title, task_description=task_description)
+    task.save()
+    return """
+    <script>
+        alert('Task submitted successfully!');
+        window.location.href = '/tasks'; 
+    </script>
+    """
