eaa226f21a1e98efb4d156a56fef027a9a5f6ae5
Cosmin445 <cosminaursulesei13@gmail.com>
2023-07-29 16:11:10 +0300

    commit 16

--- Final_Project/tasks.py
+++ Final_Project/tasks.py
@@ -1,11 +1,10 @@
-from flask import Blueprint, jsonify, app, request, render_template
+from flask import Blueprint, jsonify, request, render_template
 from model import Task
-from peewee import JOIN
 
 task_bp = Blueprint('task', __name__)
 
 @task_bp.route('/tasks', methods=['GET'])
-def get_users():
+def get_tasks():
     tasks = list(Task.select())
     return render_template('tasks.html', tasks=tasks)
 
@@ -29,3 +28,33 @@ def submit_task():
         window.location.href = '/tasks'; 
     </script>
     """
+
+@task_bp.route('/tasks/<int:task_id>', methods=['POST', 'DELETE','PUT'])
+def delete_task(task_id):
+    if request.method == 'POST':
+        if request.form.get('_method') == 'delete':
+            try:
+                task = Task.get(Task.task_id == task_id)
+                task.delete_instance()
+                return  """
+                <script>
+                    alert('Task deleted successfully!');
+                     window.location.href = '/tasks'; 
+                </script>
+                """
+            except Task.DoesNotExist:
+                return jsonify({"error": "Task not found."}), 404
+            
+        elif request.form.get('_method_1') == 'put':
+            try:
+                task = Task.get(Task.task_id == task_id)
+                task.task_state = 'Completed'
+                task.save()
+                return  """
+                <script>
+                    alert('Task updated successfully!');
+                     window.location.href = '/tasks'; 
+                </script>
+                """
+            except Task.DoesNotExist:
+                return jsonify({"error": "Task not found."}), 404
